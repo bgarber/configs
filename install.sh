8dab8f9a0bf74d2e5f3754437c168e90c1e04910
Bryan Garber da Silva <spellcasterbryan@gmail.com>
2013-05-09 10:31:11 -0300

    Updating install.sh.

--- install.sh
+++ install.sh
@@ -3,15 +3,22 @@
 # Copyright   @2013  Bryan Garber da Silva
 
 # Updating Bash configuration.
-cat $HOME/.bashrc bash/dot.bashrc >> $HOME/.bashrc
-echo "Bash script updated. You will need to restart the terminal for the \
-changes to take effect."
+cat $HOME/.bashrc bash/dot.bashrc >> $HOME/.bashrc-new
+rm $HOME/.bashrc
+mv $HOME/.bashrc-new $HOME/.bashrc
+source $HOME/.bashrc
+echo "Bash configuration script updated."
 
 # Installing Vim configuration files.
 rm -rf $HOME/.vim $HOME/.vimrc
 cp -r vim/dot.vim $HOME/.vim
 cp vim/dot.vimrc $HOME/.vimrc
 echo "Vim configuration updated."
+
+# Installing Emacs configuration files.
+rm -rf $HOME/.emacs
+cp emacs/dot.emacs $HOME/.emacs
+echo "Emacs configuration updated."
 
 # Installing Screen configuration file
 rm -rf $HOME/.screenrc
